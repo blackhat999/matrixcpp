6c4580c0f20fd100f905c4153f5109163f40582e
BlAcKhAt9(9 <hakr@protonmail.ch>
2015-07-21 01:03:09 +0000

    wrote constructor

--- matrix.cpp
+++ matrix.cpp
@@ -5,9 +5,20 @@ Matrix.cpp: code for the matrix template class
 Part of the Matrix library project
 Author: blackhat999
 
-Released under the CC0 1.0 Rights Waiver.
+Released under the CC0 1.0 Rights Waiver
+and the Unlicense.
 All rights nullified.
 */
 
 
-Matrix::
+Matrix::Matrix(long int r, long int c, bool noinit){
+    data = malloc(r*sizeof(C));
+    for(long int rl = 0; rl < r; ++rl){
+        data + rl = malloc(c*sizeof(C));
+        for(long int cl = 0, cl < c; ++cl){
+           if (noinit == true) *(*(data + rl) + cl) = 0;
+        }
+    }
+}
+
+inline Matrix::Matrix(long int r, long int c) Matrix(r,c,false);
--- matrix.h
+++ matrix.h
@@ -15,13 +15,16 @@ All rights nullified.
 
 template<class C> class Matrix{  //used template to allow selection of float, double, int, or long matrix
     private:                   // these members must be private to avoid leaking memory
-    C **data;                  // stores a pointer the type-indepen
-    int rows, cols;
+    C **data;                  // stores a pointer to the matrix
+    long int rows, cols;
     public:
-    void setRows(int r);       // These functions resize the matrix's length and width 
-    void setColumns(int c);    // by adding zero to grow the matrix and discarding data
-    void Resize(int, r int c); //setRows(r); setColumns(c);
-    
+    Matrix(long int r, long int c);
+    void setRows(long int r);       // These functions resize the matrix's length and width 
+    void setColumns(long int c);    // by adding zero to grow the matrix and discarding data
+    void resize(long int r, long int c); //setRows(r); setColumns(c);
+    long int getRows(void);        //get length
+    long int getCols(void);        //get width
+    void get()
     
 }
 
